dd05cb333ed6c9611969728cdf1a519d5a92e385
alxflam <alex.flammer.dev@outlook.com>
2023-05-12 17:13:27 +0200

    test: replace deprecated method call handler

--- test/utils/path.dart
+++ test/utils/path.dart
@@ -8,13 +8,16 @@ Future<void> setupTmpAndDocumentsDir() async {
   final directory = await Directory.systemTemp.createTemp();
 
   // Mock out the MethodChannel for the path_provider plugin.
-  const MethodChannel('plugins.flutter.io/path_provider')
-      .setMockMethodCallHandler((MethodCall? methodCall) async {
+
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMessageHandler('plugins.flutter.io/path_provider', (message) {
     // If you're getting the apps documents directory, return the path to the
     // temp directory on the test environment instead.
-    if (methodCall?.method == 'getApplicationDocumentsDirectory' ||
-        methodCall?.method == 'getTemporaryDirectory') {
-      return directory.path;
+    final methodCall = const StandardMethodCodec().decodeMethodCall(message);
+    if (methodCall.method == 'getApplicationDocumentsDirectory' ||
+        methodCall.method == 'getTemporaryDirectory') {
+      return Future.value(
+          const StandardMethodCodec().encodeSuccessEnvelope(directory.path));
     }
 
     return null;
