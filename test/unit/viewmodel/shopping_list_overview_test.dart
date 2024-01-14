2325fe39806c8c44423bd37e02a153b838ea707e
alxflam <alex.flammer.dev@outlook.com>
2024-01-14 15:47:34 +0100

    test: adapt toggling test

--- test/unit/viewmodel/shopping_list_overview_test.dart
+++ test/unit/viewmodel/shopping_list_overview_test.dart
@@ -2,6 +2,7 @@ import 'package:cookza/model/entities/mutable/mutable_shopping_list.dart';
 import 'package:cookza/services/meal_plan_manager.dart';
 import 'package:cookza/services/shopping_list/shopping_list_manager.dart';
 import 'package:cookza/viewmodel/shopping_list/shopping_list_overview.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/mockito.dart';
@@ -19,11 +20,11 @@ void main() {
 
   test('Get persisted lists - do not show data from the past', () async {
     var cut = ShoppingListOverviewModel();
-    var now = DateTime.now();
+    var now = DateUtils.dateOnly(DateTime.now());
     var entity = MutableShoppingList.newList(
         '1234',
-        now.subtract(const Duration(days: 10)),
-        now.subtract(const Duration(days: 1)));
+        DateUtils.dateOnly(now.subtract(const Duration(days: 10))),
+        DateUtils.dateOnly(now.subtract(const Duration(days: 1))));
     when(mock.shoppingListsAsList)
         .thenAnswer((realInvocation) => Future.value([entity]));
     var lists = await cut.getLists();
@@ -32,9 +33,9 @@ void main() {
 
   test('Get persisted lists - show current data', () async {
     var cut = ShoppingListOverviewModel();
-    var now = DateTime.now();
+    var now = DateUtils.dateOnly(DateTime.now());
     var entity = MutableShoppingList.newList(
-        '1234', now, now.add(const Duration(days: 6)));
+        '1234', now, DateUtils.dateOnly(now.add(const Duration(days: 6))));
     when(mock.shoppingListsAsList)
         .thenAnswer((realInvocation) => Future.value([entity]));
     var lists = await cut.getLists();
